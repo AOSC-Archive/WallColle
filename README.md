08be5ca40a1b375a20d33c6dddd296324efea9a2
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-10-02 00:28:28 +0000

    Hello world

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
-# wallcolle
-Wallpaper collection management system
+# WallColle
+
+Wallpaper collection management system.
+
+WIP. Not ready for production.
+
+## Introduction
+
+This repo is designed to be the hub of managing community-contributed wallpapers.
+
+It is tailored for [AOSC OS](https://aosc.io), but other communities may find this work helpful.
+
+## Contribution Model
+
+### Principles
+
+- Wallpapers are contributed by community members.
+- Each Wallpaper has 1 and only 1 Contributor.
+- Each Contributor has an arbitrary number of Wallpapers.
+- Each Pack contains multiple Wallpapers.
+- For each release (e.g. OS Version), a set of Wallpapers may be bundled into it. Such a release is identified as a Pack.
+
+## Wallpaper Quality Requirements
+
+- Ratios: `1:1` or `16:10`.
+- The format should be PNG, JPEG, or SVG.
+- Minimal width: 2560px for `1:1`; 3200px for `16:10`.
+- The colorspace should be sRGB.
+- The color depth should be 8 bits.
+
+## Contribute Wallpapers
+
+### Copyright
+
+- You should own the to-be-submitted image, or you can prove that any person can redistribute the image under a specific license.
+
+### 1. Fork This Repo
+
+Fork this repo to your own account and make changes there.
+
+The following steps are supposed to be executed in your own repository.
+
+### 2. Create Your Directory & Manifest
+
+Suppose that your GitHub username is `myname`.
+
+At `/contributors/myname/me.json`, you should write a JSON configuration which looks like this:
+
+```json
+{
+    "uname": "myname",
+    "name": "My Name",
+    "uri": "mailto:myname@exmaple.com",
+    "wallpapers": [
+        {
+            "i": 0,
+            "f": "png",
+            "t": "Image_Title",
+            "l": "cc-by-nd-4.0",
+            "tags": [ "Abstract" ,"Nature", "Forest" ]
+        }
+    ]
+}
+```
+
+Field           | Description
+--------------- | -----------
+`uname`         | Your GitHub username.
+`name`          | Your human-friendly name.
+`uri`           | Your email address () or website.
+`wallpapers`    | An array of your wallpapers.
+
+For each entry in `wallpapers` field:
+
+Field   | Description
+------- | -----------
+`i`     | The index of this image.
+`f`     | The format of this image. Must be 3 lowercase characters. Can be `png`, `jpg`, or `svg`.
+`t`     | The title of this image. Will be used for human-friendly display purposes. Can include letters, numbers, and hyphens. Must be unique across contributors. Use title capitalization.
+`l`     | License identifier. You can find the list of acceptable licenses in another section later.
+`tags`  | Optional field. An array of tags. Each tag can only contain lowercase letters. You can find the list of acceptable tags in another section later.
+
+### 3. Put Your Image
+
+Since this is your initial contribution, your directory should be empty.
+
+Suppose that your contribution is a PNG file. You may put it in your directory and its path should be `/contributors/myname/0.png`.
+
+The index should be autoincremental from 0. All formats share the same counter.
+
+## Packs
+
+### Basics
+
+Each pack is represented by a file in `/packs`
+
+## Contribute Code
+
+There is no limit!
+
+## Copyright
+
+Copyright (c) 2020 Neruthes <i@neruthes.xyz> and 0 other contributors.
+
+The Programs in this repository are released under GNU GPLv2.
+
+## Contributors
+
+## Appendix 1: Acceptable Tags
+
+- Abstract
+  - Geometry
+  - Laser
+- Civil
+  - Countryside
+  - Cyberpunk
+  - Cyberpunk
+  - Metropolis
+- Fantasy
+- Illustration
+- Legacy
+- Nature
+  - Desert
+  - Forest
+  - Jungle
+  - Mountain
+  - Oasis
+  - Plant
+  - Water
---
+++ make.js
@@ -0,0 +1,148 @@
+// Project:     WallColle
+// Repo:        https://github.com/neruthes/wallcolle
+// File:        make.js
+// Author:      Neruthes <i@neruthes.xyz> (2020 Oct 01)
+// License:     GNU GPLv2
+
+// --------------------------------------
+// Dependencies
+const fs = require('fs');
+
+// --------------------------------------
+// Arguments initialization
+const PACKNAME = process.argv[2];
+const CONFIG = require('./config.json');
+
+// --------------------------------------
+// Begin function def
+
+const padright = function (str, len) {
+    if (str.length >= len) {
+        return str;
+    };
+    return str + (new Array(len-str.length)).fill(' ').join('');
+};
+
+const getUserManifest = function (username) {
+    let userdata = JSON.parse(fs.readFileSync(`./contributors/${username}/me.json`).toString());
+    userdata.wallpapers.map(function (x, i) {
+        ['uname','name','uri'].map(function (keyname) {
+            userdata.wallpapers[i][keyname] = userdata[keyname];
+        });
+    });
+    return userdata;
+};
+
+const buildDatabase = function () {
+    let db = {};
+    let users = fs.readdirSync('./contributors').filter(function (x) { return x[0] !== '.' });
+    users.forEach(function (username) {
+        db[username] = getUserManifest(username);
+    });
+    console.log('Building database...');
+    // console.log(db);
+    fs.writeFileSync('./db.json', JSON.stringify(db, '\t', 4));
+    return db;
+};
+
+const renderPackManifest = function (obj) {
+    const renderLines = function (entries) {
+        return entries.map(function (entry) {
+            return [
+                padright(entry.t, 36),
+                padright(entry.name, 20),
+                padright(entry.l, 2)
+            ].join('');
+        }).join('');
+    };
+    return `
+PackName   :   ${PACKNAME}
+DateTime   :   ${obj.date.slice(0, 19).replace('T', ' ')}
+Quantity   :   ${obj.entries.length}
+
+Comments   :
+${obj.comments.join('\n').replace(/# /g, '#     ')}
+
+Title                               Contributor         License
+===============================================================================
+` + renderLines(obj.entries) + '\n';
+};
+
+const parsePackDef = function (deffile) {
+    let rawdata = deffile.trim().split('\n');
+    let catalog = [];
+    let manifestObj = {
+        title: PACKNAME,
+        date: (new Date()).toISOString(),
+        comments: [],
+        entries: []
+    };
+    rawdata.forEach(function (line, i) {
+        if (line.length > 2 && line.indexOf('# ') === -1) {
+            // Not comment
+            let mymatch = line.match(/^([0-9A-Za-z_\-]+)\:(\d+)$/);
+            if (mymatch) {
+                // Yes, good declaration line.
+                catalog.push({
+                    uname: mymatch[1],
+                    index: mymatch[2]
+                });
+                manifestObj.entries.push(
+                    db[mymatch[1]].wallpapers[mymatch[2]]
+                );
+            } else {
+                console.error(`Error: Bad declaration in line ${i}. Please fix before proceeding.`);
+                console.error(`Statement: "${line}"`);
+                process.exit(1);
+            };
+        } else if (line.indexOf('# ') === 0) {
+            // Comment
+            manifestObj.comments.push(line);
+        };
+    });
+    return {
+        catalog: catalog,
+        manifestObj: manifestObj,
+        manifestStr: renderPackManifest(manifestObj)
+    };
+};
+
+// --------------------------------------
+// Begin controller logic
+
+if (!fs.existsSync('./dist')) {
+    console.log(`Initializing "dist" directory...`);
+    fs.mkdirSync('./dist');
+} else {
+    fs.readdir('./dist', function (err, stdin, stderr) {
+        if (!err) {
+            console.log(`Cleaning existing "dist" directory...`);
+            stdin.forEach(function (filename) {
+                fs.unlinkSync(`./dist/${filename}`);
+            });
+        };
+    });
+};
+
+let db = buildDatabase();
+
+console.log(`Trying to create pack for "${PACKNAME}"...\n`);
+
+fs.readFile(`./packs/${PACKNAME}`, function (err, stdin, stderr) {
+    if (err) {
+        console.error(`Error: Cannot make "packs/${PACKNAME}".`);
+        process.exit(1);
+    };
+    let packdata = parsePackDef(stdin.toString());
+    fs.writeFileSync(`./dist/manifest.txt`, packdata.manifestStr);
+
+    packdata.manifestObj.entries.forEach(function (entryObj) {
+        let srcpath = `./contributors/${entryObj.uname}/${entryObj.i}.${entryObj.f}`;
+        let destpath = `./dist/${entryObj.t.replace(/ /g, '_')}.${entryObj.f}`;
+        console.log(`copying: `, srcpath, destpath);
+        fs.copyFileSync(srcpath, destpath);
+    });
+
+    console.log(packdata.manifestStr);
+    console.log(`\n\nSuccessfully built the pack "${PACKNAME}" with ${packdata.manifestObj.entries.length} wallpapers.\n`);
+});
