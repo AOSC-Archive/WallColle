8f09efad4eab9aa6172d820646bbb98aa6531410
Neruthes <i@neruthes.xyz>
2020-10-23 07:51:26 +0000

    Minor bug fix

---
+++ clean.sh
@@ -0,0 +1,2 @@
+rm -r $PWD/usr 2> /dev/null
+rm -r /tmp/WallColle_ea9510656e3a43d8b037dd34490ad52f 2> /dev/null
--- make.js
+++ make.js
@@ -207,7 +207,8 @@ const finisherScript = function (manifestObj) {
             };
             exec(`convert ${srcimgpathold} -resize x1200 -quality 80 ${srcimgpath}`);
             exec(`pngquant 256 ${srcimgpath} -o ${srcimgpath}.optimized`);
-            exec(`rm -r /tmp/WallColle_${UUID}`);
+            srcimgpath = srcimgpath + '.optimized'
+            // exec(`rm -r /tmp/WallColle_${UUID}`);
             img.f = 'png';
         };
 
