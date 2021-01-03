418963dc83e7516fb93e232196033267c2075bcc
Neruthes <i@neruthes.xyz>
2021-01-03 07:30:02 +0000

    Better stdname rule

--- make.js
+++ make.js
@@ -186,7 +186,7 @@ const finisherScript = function (manifestObj) {
     // console.log(manifestObj);
     manifestObj.entries.forEach(function (img) {
         // console.log(img);
-        let stdname = `${PACKNAME}.${img.t.toLowerCase().replace(/ /g, '_')}.${img.uname}`;
+        let stdname = `${PACKNAME}.${img.t.toLowerCase().replace(/[\s\.\-]/g, '_').replace(/_+/g, '_')}.${img.uname}`;
         let srcimgpath = `./contributors/${img.uname}/${img.i}.${img.f}`;
         // console.log(stdname);
         // console.log(srcimgpath);
